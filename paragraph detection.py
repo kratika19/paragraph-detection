b4712981d7275c19e00b8b68a2ddee99fc0b0cd2
kratika19 <56781757+kratika19@users.noreply.github.com>
2020-04-07 17:37:08 +0530

    Add files via upload

---
+++ paragraph detection.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 11:50:43 2020
+
+@author: krati
+"""
+
+import cv2
+
+# Load image, grayscale, Gaussian blur, adaptive threshold
+image = cv2.imread(r"C:\Users\krati\Desktop\11.png")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray, (5,5), 0)
+thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,15,30)
+
+# Dilate to combine adjacent text contours
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+dilate = cv2.dilate(thresh, kernel, iterations=3)
+
+# Find contours, highlight text areas, and extract ROIs
+cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[2]
+
+ROI_number = 0
+for c in cnts:
+    area = cv2.contourArea(c)
+    if area > 1000:
+        x,y,w,h = cv2.boundingRect(c)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0,0,0), 2)
+        # ROI = image[y:y+h, x:x+w]
+        # cv2.imwrite('ROI_{}.png'.format(ROI_number), ROI)
+        # ROI_number += 1
+
+cv2.imshow('thresh', thresh)
+cv2.imshow('dilate', dilate)
+cv2.imshow('image1', image)
+cv2.imshow('BLUR', blur)
+cv2.imwrite("FINAL.png",image)
+cv2.waitKey()
